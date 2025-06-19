c16571da5f3cf30faf0c9b1171b3febaf48208bf
MuraliVepamani <murali.vepamani@upgrad.com>
2025-06-19 11:47:51 +0530

    Initial commit

---
+++ app/src/main/java/com/chalkboard/data/models/DrawStroke.kt
@@ -0,0 +1,15 @@
+package com.chalkboard.data.models
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+
+/**
+ * Simple stroke data class with only essential properties
+ */
+data class DrawStroke(
+    val path: Path = Path(),
+    val color: Color = Color.Black,
+    val strokeWidth: Float = 6f,
+    val isEraser: Boolean = false
+)
+
